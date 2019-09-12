a47dd7c47f8774f87354b41d6a02957e0a8ec12f
wood <wljlinksly@gmail.com>
2019-09-12 10:32:14 +0800

    bug fixed during output of job order when some stage have been completely finished

--- src/task.cpp
+++ src/task.cpp
@@ -20,26 +20,48 @@ void Task::addJob(Job* j, int i){
 }
 
 std::ostream& operator<<(std::ostream& os, const Task& t){
+    std::vector<std::vector<std::string>> udlist;
+    int udonen = 0; // undone stage 
+    bool stgdone = true; // stage all done or not
+    // output undone job definition
     for(auto& e: t.joblist){
+        stgdone = true;
         for(auto& f: e){
-            os << (*f);
+            if(f->status.second != "done"){
+                stgdone = false;
+                os << (*f);
+            }
         }
+        if(!stgdone) ++udonen;
     }
+    // logdir 
     if(!t.logdir.second.empty()){
         os << "  " << t.logdir.first << " " << t.logdir.second << "\n";
     }
+    // skip one stage task
     if(t.joblist.size() <= 1){
         return os;
     }
-    for(size_t i = 1; i < t.joblist.size(); ++i){
-        for(auto& e: t.joblist[i-1]){
-            for(auto& f: t.joblist[i]){
-                if(e->status.second != "done" && f->status.second != "done"){
-                    os << "order  " << e->name.second << " before " << f->name.second << "\n";
-                }
+    // reconstruct undone task joblist
+    udlist.resize(udonen);
+    udonen = 0;
+    for(size_t i = 0; i < t.joblist.size(); ++i){
+        stgdone = true;
+        for(auto& e: t.joblist[i]){
+            if(e->status.second != "done"){
+                udlist[udonen].push_back(e->name.second);
+                stgdone = false;
+            }
+        }
+        if(!stgdone) ++udonen;
+    }
+    // output job order
+    for(size_t i = 1; i < udlist.size(); ++i){
+        for(auto& e: udlist[i-1]){
+            for(auto& f: udlist[i]){
+                os << "order  " << e << " before " << f << "\n";
             }
         }
     }
     return os;
  }
-
